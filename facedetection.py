84519db25793aa1a9553a092d2507a4d932ea33c
robertchoi <robertchoimail@gmail.com>
2020-07-09 00:06:30 +0900

    update face

--- facedetection.py
+++ facedetection.py
@@ -20,11 +20,8 @@ while True:
     img = cv2.flip(img, -1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(
-        gray,
-        
-        scaleFactor=1.2,
-        minNeighbors=5
-        ,     
+        gray,scaleFactor=1.2,
+        minNeighbors=5,     
         minSize=(20, 20)
     )
 
