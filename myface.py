b89d4cce23f2236f2a7a687c30eabaa80f433620
robertchoi <robertchoimail@gmail.com>
2020-07-09 00:26:22 +0900

    update code

--- myface.py
+++ myface.py
@@ -1,58 +1,33 @@
-# import the necessary packages
-
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
+import numpy as np
 import cv2
 
+# multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
-def detect(img, cascade):
-    rects = cascade.detectMultiScale(img, scaleFactor=1.3, minNeighbors=4, minSize=(30, 30),
-                                     flags=cv2.CASCADE_SCALE_IMAGE)
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-    if len(rects) == 0:
-        return []
+cap = cv2.VideoCapture(0)
 
-    rects[:,2:] += rects[:,:2]
-    return rects
-
-
-
-def draw_rects(img, rects, color):
-    for x1, y1, x2, y2 in rects:
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
-
-
-
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-cascade = cv2.CascadeClassifier("./data/haarcascades/haarcascade_frontalface_alt.xml")
-
-
-
-# allow the camera to warmup
-time.sleep(0.1)
-
-
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    img = frame.array
+while 1:
+    ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    rects = detect(gray, cascade)
-    vis = img.copy()
-    draw_rects(vis, rects, (0, 255, 0))
-    # show the frame
-    cv2.imshow("Frame", vis)
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
         break
+
+cap.release()
+cv2.destroyAllWindows()
