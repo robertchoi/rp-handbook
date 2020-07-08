a8dc9fbf2f2e019106fc3d48ee1b2ed6bafcc4b7
robertchoi <robertchoimail@gmail.com>
2020-07-08 22:25:28 +0900

    update pycam.py

---
+++ pycam.py
@@ -0,0 +1,12 @@
+
+
+import picamera, sys  # 카메라랑 시스템을 관리하는 라이브러리를 사용합니다.
+
+
+with picamera.PiCamera() as camera
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    camera.start_recording('cos.h264')
+    camera.wait_recording(30)
+    camera.stop_recording()
+    camera.stop_preview()
