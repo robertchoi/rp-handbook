5483a5fb4eb62714da6b195a910e8530258c66a4
robertchoi <robertchoimail@gmail.com>
2020-07-08 23:04:37 +0900

    update code

--- cv_test.py
+++ cv_test.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-img_file = “./pic.jpg"
+img_file = "./pic.jpg"
 img = cv2.imread(img_file)
 
-cv2.imshow(‘IMG', img)
+cv2.imshow('IMG', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
